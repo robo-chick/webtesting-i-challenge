3ae9fde089d6d7625a877b3cce3f93f6069764ae
Tasha <tashingsworth@gmail.com>
2020-09-10 18:59:29 -0400

    all tests passing, MVP met

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -3,21 +3,28 @@ module.exports = {
   fail,
   repair,
   get,
-};
+}
 
 function success(item) {
-  const newEnhancementLvl1 = item.enhancement < 20 ? item.enhancement + 1 : 20
-  return {...item, enhancement: newEnhancementLvl1}
+  const newEnhancementLvl = item.enhancement < 20 ? item.enhancement + 1 : 20
+  return { ...item, enhancement: newEnhancementLvl }
 }
 
 function fail(item) {
-  return { ...item };
+ 
+  let newDurability = item.durability - 1 // set the base case(numbers greater than 16)
+  if (item.enhancement < 15) newDurability = item.durability - 5 // first check and reassignment
+  else if (item.enhancement === 15 || item.enhancement === 16) newDurability = item.durability - 10 // second check and reassignment
+
+  return { ...item, durability: newDurability }
 }
 
 function repair(item) {
-  return { ...item, durability: 100 };
+  return { ...item, durability: 100 }
 }
 
 function get(item) {
-  return { ...item };
+  let name = item.name
+  if (item.enhancement > 0) name = `[+${item.enhancement}] ${item.name}`
+  return { ...item, name: name }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -5,19 +5,51 @@ describe("Repairs item", () => {
     it("Should set items durability to 100", () => {
         const item = {enhancement: 10, durability: 20}
         const repairedItem = enhancer.repair(item)
-        expect(repairedItem.durability).toEqual(100)
+        expect(repairedItem.durability).toBe(100)
     })
 })
 
-describe("Enahnces item", () => {
-    // on success enhancement increases by +1, limiting at 20
-    it("should succeed in enhancement", () => {
-        const item = {
-            name: "sword",
-            enhancement: 20, 
-            durability: 100
-        }
+describe("Enhances item", function () {
+    // on success enhancement increases by one limiting at 20
+    // on fail enhancement as e < 15 durability as d -= 5 || e === 15 e === 16 d -= 10 || d -= 1
+    it("Should succeed in enhancement", () => {
+        const item = {enhancement: 20, durability: 100}
         const newItem = enhancer.success(item)
         expect(newItem.enhancement).toBe(20)
     })
+    it("Should succeed in enhancement setting enhancement to 11", () => {
+        const item = {enhancement: 10, durability: 100}
+        const newItem = enhancer.success(item)
+        expect(newItem.enhancement).toBe(11)
+    })
+    it("Should fail in enhancement setting durability to 95", () => {
+        const item = {enhancement: 10, durability: 100}
+        const newItem = enhancer.fail(item)
+        expect(newItem.durability).toBe(95)
+    })
+    it("Should fail in enhancement setting durability to 90", () => {
+        const item = {enhancement: 15, durability: 100}
+        const newItem = enhancer.fail(item)
+        expect(newItem.durability).toBe(90)
+    })
+    it("Should fail in enhancement setting durability to 99", () => {
+        const item = {enhancement: 20, durability: 100}
+        const newItem = enhancer.fail(item)
+        expect(newItem.durability).toBe(99)
+    })
 })
+
+describe("Gets item", () => {
+    it("Should get an item with the name set to [+7] Bronze Sword", () => {
+        const item = {enhancement: 7, durability: 100, name: "Bronze Sword"}
+        const newItem = enhancer.get(item)
+        expect(newItem.name).toBe("[+7] Bronze Sword")
+    })
+
+    it("Should get an item with the name set to Bronze Sword", () => {
+        const item = {enhancement: 0, durability: 100, name: "Bronze Sword"}
+        const newItem = enhancer.get(item)
+        expect(newItem.name).toBe("Bronze Sword")
+    })
+})
+
